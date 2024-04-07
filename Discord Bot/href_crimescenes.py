6d39e36692439ece1addd591d3b1bb959f86f50d
AISHWARYA SHARMA <148491110+Aishrma@users.noreply.github.com>
2024-04-07 15:26:51 +0530

    added relative paths of crimescenes

---
+++ Discord Bot/href_crimescenes.py
@@ -0,0 +1,12 @@
+images_list = [
+   "images_crimescenes/scene1.png" ,
+    "images_crimescenes/scene2.png" ,    
+    "images_crimescenes/scene3.png" ,
+    "images_crimescenes/scene4.png" ,
+    "images_crimescenes/scene5.png" ,
+    "images_crimescenes/scene6.png" ,
+    "images_crimescenes/scene7.png" ,
+    "images_crimescenes/scene8.png" ,
+    "images_crimescenes/scene9.png" ,
+    "images_crimescenes/scene10.png"
+]
