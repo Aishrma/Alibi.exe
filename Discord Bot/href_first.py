3413608e354c8ee568ed15d6b04f4631cd995318
Kavya Gupta <165313930+kavyagupta3011@users.noreply.github.com>
2024-04-07 18:10:56 +0530

    relative links for first images

---
+++ Discord Bot/href_first.py
@@ -0,0 +1,7 @@
+first_list=[
+    "images_first/ceaser.jpg",
+    "images_first/hangman.jpg",
+    "images_first/morse.jpg",
+    "images_first/murder_mystery.jpg",
+    "images_first/scramble.jpg"
+]
